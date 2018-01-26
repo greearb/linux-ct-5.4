772711e9b381d13d79db9db7491955737aa649d2
Ben Greear <greearb@candelatech.com>
2018-01-25 16:42:09 -0800

    ath10k: Correctly report tx-rate of 48Mbps.
    48Mbps is special in that it has a rate-code of 0x0, and flags == 0
    means success, so the old code to check whether tx-rate status was set
    or not did not properly match.  So, remember if we have received *any*
    tx-rate info, and if so, we can assume that we will continue receiving it
    since that is how the CT firmware works (and stock firmware never sets
    any tx-rate info at all).
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -1115,6 +1115,7 @@ struct ath10k {
 
 	enum ath10k_hw_rev hw_rev;
 	u16 dev_id;
+	bool ok_tx_rate_status; /* Firmware is sending tx-rate status?  (CT only) */
 	bool fw_powerup_failed; /* If true, might take reboot to recover. */
 	u32 chip_id;
 	enum ath10k_dev_type dev_type;
